cf2e75fb9d1d0d8c38c19addc9bd9ea2d20d9e3e
trust-me-im-an-engineer <isavelij4@gmail.com>
2026-02-20 19:42:38 +0300

    golangci-lint plugin

---
+++ plugin/plugin.go
@@ -0,0 +1,30 @@
+package plugin
+
+import (
+	"github.com/golangci/plugin-module-register/register"
+	"golang.org/x/tools/go/analysis"
+
+	"github.com/trust-me-im-an-engineer/logcheck/analyser"
+)
+
+func init() {
+	register.Plugin("logcheck", New)
+}
+
+// PluginWrapper — обертка, которую требует golangci-lint
+type PluginWrapper struct {
+	// здесь можно хранить настройки (Bonus Task 1)
+}
+
+func New(settings any) (register.LinterPlugin, error) {
+	// В будущем здесь можно десериализовать настройки через register.DecodeSettings
+	return &PluginWrapper{}, nil
+}
+
+func (p *PluginWrapper) BuildAnalyzers() ([]*analysis.Analyzer, error) {
+	return []*analysis.Analyzer{analyser.Analyzer}, nil
+}
+
+func (p *PluginWrapper) GetLoadMode() string {
+	return register.LoadModeTypesInfo
+}
